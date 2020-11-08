dbb595341b55ebebf43e734b26b697ef8d8815a2
Diogo Parente <diogo.parente@talkdesk.com>
2020-11-08 18:10:38 +0000

    Remove Dark mode

--- src/components/layout/Page.jsx
+++ src/components/layout/Page.jsx
@@ -1,21 +1,9 @@
-import React, { useContext, useState } from "react";
-import styled, { createGlobalStyle } from "styled-components";
+import React from "react";
+import styled from "styled-components";
 
-import theme from "../../../config/theme";
-import ThemeContext from "../../context/theme/ThemeContext";
-import ThemeToggler from "../../context/theme/ThemeToggler";
 import Header from "./Header";
 import Helmet from "./Helmet";
-
-const GlobalStyle = createGlobalStyle`
-  body {
-    font-family: 'Lato', sans-serif;
-    background: ${(props) => props.theme.background};
-    color: ${(props) => props.theme.color};
-    min-width: 300px;
-    margin: 10px;
-  }
-`;
+import ThemeToggler from "./ThemeToggler";
 
 const PageContainer = styled.div`
   min-height: calc(100vh);
@@ -24,28 +12,13 @@ const PageContainer = styled.div`
 `;
 
 const Page = ({ children }) => {
-  const themeContext = useContext(ThemeContext);
-
-  const toggleTheme = (contextTheme) =>
-    contextTheme.isDark
-      ? setState({ ...state, theme: theme.light })
-      : setState({ ...state, theme: theme.dark });
-
-  const [state, setState] = useState({
-    theme: themeContext.theme,
-    toggleTheme: toggleTheme,
-  });
-
   return (
-    <ThemeContext.Provider value={state}>
-      <PageContainer>
-        <Helmet />
-        <GlobalStyle theme={state.theme} />
-        <Header />
-        <ThemeToggler />
-        {children}
-      </PageContainer>
-    </ThemeContext.Provider>
+    <PageContainer>
+      <Helmet />
+      <Header />
+      <ThemeToggler />
+      {children}
+    </PageContainer>
   );
 };
 
--- src/context/theme/ThemeContext.jsx
+++
@@ -1,9 +0,0 @@
-import { createContext } from "react";
-import theme from "../../../config/theme";
-
-const ThemeContext = createContext({
-  theme: theme.light,
-  toggleTheme: () => {},
-});
-
-export default ThemeContext;
